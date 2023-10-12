08dbdfa001e0749c58c527ab5f28f6662fef4b28
Patryk <apatryk19@gmail.com>
2023-10-12 11:50:26 +0200

    resolve quiz

--- frontend/src/app/create/page.js
+++ frontend/src/app/create/page.js
@@ -2,166 +2,166 @@
 import { useState } from 'react';
 
 export default function CreateQuiz() {
-  const [quizData, setQuizData] = useState({
-    title: '',
-    description: '',
-    status: 'published',
-    userId: 2, // You can retrieve this value from local storage or a cookie
-    categoriesIds: [],
-    questions: [
-      {
-        question: '',
-        answers: [
-          {
+    const [quizData, setQuizData] = useState({
+        title: '',
+        description: '',
+        status: 'published',
+        userId: 2, // You can retrieve this value from local storage or a cookie
+        categoriesIds: [],
+        questions: [
+            {
+                question: '',
+                answers: [
+                    {
+                        answer: '',
+                        isCorrect: false,
+                    },
+                ],
+            },
+        ],
+    });
+
+    const handleQuestionAdd = () => {
+        setQuizData({
+            ...quizData,
+            questions: [
+                ...quizData.questions,
+                {
+                    question: '',
+                    answers: [
+                        {
+                            answer: '',
+                            isCorrect: false,
+                        },
+                    ],
+                },
+            ],
+        });
+    };
+
+    const handleAnswerAdd = (questionIndex) => {
+        const updatedQuestions = [...quizData.questions];
+        updatedQuestions[questionIndex].answers.push({
             answer: '',
             isCorrect: false,
-          },
-        ],
-      },
-    ],
-  });
+        });
+        setQuizData({
+            ...quizData,
+            questions: updatedQuestions,
+        });
+    };
 
-  const handleQuestionAdd = () => {
-    setQuizData({
-      ...quizData,
-      questions: [
-        ...quizData.questions,
-        {
-          question: '',
-          answers: [
-            {
-              answer: '',
-              isCorrect: false,
-            },
-          ],
-        },
-      ],
-    });
-  };
+    const handleSubmit = async () => {
+        const accessToken = localStorage.getItem('token');
+        // Send the JSON data to the '/quizzes' endpoint using fetch
+        try {
+            const response = await fetch('http://localhost:3000/quizzes', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${accessToken}`,
+                },
+                body: JSON.stringify(quizData),
+            });
+            // Handle the response as needed
+        } catch (error) {
+            // Handle errors
+        }
+    };
 
-  const handleAnswerAdd = (questionIndex) => {
-    const updatedQuestions = [...quizData.questions];
-    updatedQuestions[questionIndex].answers.push({
-      answer: '',
-      isCorrect: false,
-    });
-    setQuizData({
-      ...quizData,
-      questions: updatedQuestions,
-    });
-  };
-
-  const handleSubmit = async () => {
-    const accessToken = localStorage.getItem('token');
-    // Send the JSON data to the '/quizzes' endpoint using fetch
-    try {
-      const response = await fetch('http://localhost:3000/quizzes', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify(quizData),
-      });
-      // Handle the response as needed
-    } catch (error) {
-      // Handle errors
-    }
-  };
-
-  return (
-    <div>
-      <h1>Create Quiz</h1>
-      <form>
-        <label>
-          Title:
-          <input
-            type="text"
-            value={quizData.title}
-            onChange={(e) => setQuizData({ ...quizData, title: e.target.value })}
-          />
-        </label>
-
-        <label>
-          Description:
-          <textarea
-            value={quizData.description}
-            onChange={(e) => setQuizData({ ...quizData, description: e.target.value })}
-          />
-        </label>
-
-        <label>
-          Categories:
-          <select
-            multiple
-            value={quizData.categoriesIds}
-            onChange={(e) => setQuizData({ ...quizData, categoriesIds: Array.from(e.target.selectedOptions, (option) => option.value) })}
-          >
-            {/* Populate categories from a prefetch list */}
-            <option value="1">Category 1</option>
-            {/* Add more category options here */}
-          </select>
-        </label>
-
-        {quizData.questions.map((question, questionIndex) => (
-          <div key={questionIndex}>
-            <label>
-              Question:
-              <input
-                type="text"
-                value={question.question}
-                onChange={(e) => {
-                  const updatedQuestions = [...quizData.questions];
-                  updatedQuestions[questionIndex].question = e.target.value;
-                  setQuizData({ ...quizData, questions: updatedQuestions });
-                }}
-              />
-            </label>
-
-            {question.answers.map((answer, answerIndex) => (
-              <div key={answerIndex}>
+    return (
+        <div>
+            <h1>Create Quiz</h1>
+            <form>
                 <label>
-                  Answer:
-                  <input
-                    type="text"
-                    value={answer.answer}
-                    onChange={(e) => {
-                      const updatedQuestions = [...quizData.questions];
-                      updatedQuestions[questionIndex].answers[answerIndex].answer = e.target.value;
-                      setQuizData({ ...quizData, questions: updatedQuestions });
-                    }}
-                  />
+                    Title:
+                    <input
+                        type="text"
+                        value={quizData.title}
+                        onChange={(e) => setQuizData({ ...quizData, title: e.target.value })}
+                    />
                 </label>
 
                 <label>
-                  Correct:
-                  <input
-                    type="checkbox"
-                    checked={answer.isCorrect}
-                    onChange={(e) => {
-                      const updatedQuestions = [...quizData.questions];
-                      updatedQuestions[questionIndex].answers[answerIndex].isCorrect = e.target.checked;
-                      setQuizData({ ...quizData, questions: updatedQuestions });
-                    }}
-                  />
+                    Description:
+                    <textarea
+                        value={quizData.description}
+                        onChange={(e) => setQuizData({ ...quizData, description: e.target.value })}
+                    />
                 </label>
-              </div>
-            ))}
 
-            <button type="button" onClick={() => handleAnswerAdd(questionIndex)}>
-              Add New Answer
-            </button>
-          </div>
-        ))}
+                <label>
+                    Categories:
+                    <select
+                        multiple
+                        value={quizData.categoriesIds}
+                        onChange={(e) => setQuizData({ ...quizData, categoriesIds: Array.from(e.target.selectedOptions, (option) => option.value) })}
+                    >
+                        {/* Populate categories from a prefetch list */}
+                        <option value="1">Category 1</option>
+                        {/* Add more category options here */}
+                    </select>
+                </label>
 
-        <button type="button" onClick={handleQuestionAdd}>
-          Add New Question
-        </button>
+                {quizData.questions.map((question, questionIndex) => (
+                    <div key={questionIndex}>
+                        <label>
+                            Question:
+                            <input
+                                type="text"
+                                value={question.question}
+                                onChange={(e) => {
+                                    const updatedQuestions = [...quizData.questions];
+                                    updatedQuestions[questionIndex].question = e.target.value;
+                                    setQuizData({ ...quizData, questions: updatedQuestions });
+                                }}
+                            />
+                        </label>
 
-        <button type="button" onClick={handleSubmit}>
-          Create Quiz
-        </button>
-      </form>
-    </div>
-  );
+                        {question.answers.map((answer, answerIndex) => (
+                            <div key={answerIndex}>
+                                <label>
+                                    Answer:
+                                    <input
+                                        type="text"
+                                        value={answer.answer}
+                                        onChange={(e) => {
+                                            const updatedQuestions = [...quizData.questions];
+                                            updatedQuestions[questionIndex].answers[answerIndex].answer = e.target.value;
+                                            setQuizData({ ...quizData, questions: updatedQuestions });
+                                        }}
+                                    />
+                                </label>
+
+                                <label>
+                                    Correct:
+                                    <input
+                                        type="checkbox"
+                                        checked={answer.isCorrect}
+                                        onChange={(e) => {
+                                            const updatedQuestions = [...quizData.questions];
+                                            updatedQuestions[questionIndex].answers[answerIndex].isCorrect = e.target.checked;
+                                            setQuizData({ ...quizData, questions: updatedQuestions });
+                                        }}
+                                    />
+                                </label>
+                            </div>
+                        ))}
+
+                        <button type="button" onClick={() => handleAnswerAdd(questionIndex)}>
+                            Add New Answer
+                        </button>
+                    </div>
+                ))}
+
+                <button type="button" onClick={handleQuestionAdd}>
+                    Add New Question
+                </button>
+
+                <button type="button" onClick={handleSubmit}>
+                    Create Quiz
+                </button>
+            </form>
+        </div>
+    );
 }
--- frontend/src/app/quiz/[id]/page.js
+++ frontend/src/app/quiz/[id]/page.js
@@ -1,18 +1,88 @@
-export default async function Page({ params }) {
-  const post = await getPost(params);
-  
+"use client"
+import { useEffect, useState } from "react";
+
+
+export default function Page({ params }) {
+  const [quizData, setQuizData] = useState(null);
+  const [userAnswers, setUserAnswers] = useState([]);
+
+
+  useEffect(() => {
+    console.log(params.id)
+    fetch("http://localhost:3000/quizzes/single/5")
+    .then((res)=>res.json())
+    .then((data)=>{
+      setQuizData(data)
+    })
+  }, []);
+
+  const handleAnswerSelection = (questionId, chosenAnswerId) => {
+    setUserAnswers((prevAnswers) => [
+      ...prevAnswers,
+      { questionId, chosenAnswerId },
+    ]);
+  };
+
+  const handleSubmit = () => {
+    // Create a JSON object with user's answers
+    const userAnswersJSON = {
+      quizId: quizData.id,
+      questions: userAnswers,
+    };
+    // Send the JSON object to the server for scoring (you'll need to implement this part on the server)
+    // Example AJAX request
+    fetch('http://localhost:3000/quizzes/validate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userAnswersJSON),
+    })
+      .then((response) => response.json())
+      .then((score) => {
+        alert(`Your score is: ${score.score}`);
+      })
+      .catch((error) => {
+        console.error('Error scoring quiz:', error);
+      });
+  };
+
+
+
   return(
-    <>
-    <div>My Post: {params.id}</div>
-    <div>{post.title}</div>
-    <div>{post.description}</div>
-    </>
+    <div>
+      {quizData === null && <p>Loading...</p>}
+
+      {quizData !== null && (
+        <>
+      <h1>{quizData.title}</h1>
+      <p>{quizData.description}</p>
+      <div>
+        {quizData.questions.map((question) => (
+          <div key={question.id}>
+            <p>{question.question}</p>
+            {question.answers.map((answer) => (
+              <div key={answer.id}>
+                <input
+                  type="radio"
+                  name={`question-${question.id}`}
+                  value={answer.id}
+                  onChange={() =>
+                    handleAnswerSelection(question.id, answer.id)
+                  }
+                />
+                <label>{answer.answer}</label>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <button onClick={handleSubmit}>Submit</button>
+      </>
+      )}
+      </div>
+
   )
 
 }
 
-const getPost = async ({id})  => {
-  const data = await fetch(process.env.KOGOOT_BACK+'/quizzes/single/'+id);
-  const posts = await data.json();
-  return posts;
-};
